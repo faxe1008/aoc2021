5ebea122979087a4d6db033b24bff2ab278cbdd3
blatzfab <fabian.blatz@hansgrohe.com>
2021-12-21 21:06:25 +0100

    Solution two for crabs

--- 7-crab-blast/src/main.rs
+++ 7-crab-blast/src/main.rs
@@ -18,7 +18,10 @@ fn median(list: &[u32]) -> f64 {
 
 fn get_fuel_for_pos(list: &[u32], pos: u32) -> u32 {
     list.iter()
-        .map(|&y| (y as i32 - pos as i32).abs() as u32)
+        .map(|&y| {
+            let distance = (y as i64 - pos as i64).abs();
+            ((distance.pow(2) + distance) / 2) as u32
+        })
         .sum()
 }
 
@@ -33,14 +36,7 @@ fn main() {
         text.trim().split(',').map(|y| y.parse().unwrap()).collect();
     crabs_positions.sort();
 
-    let median = median(&crabs_positions);
-    println!(
-        "MEDIAN: {}, {}",
-        median,
-        get_fuel_for_pos(&crabs_positions, median as u32)
-    );
-
-    /*let max_distance = *crabs_positions.iter().max().unwrap();
+    let max_distance = *crabs_positions.iter().max().unwrap();
     let mut cheapest_distance = 0;
     let mut lowest_fuel_cost = u32::max_value();
     for i in 0..max_distance {
@@ -50,5 +46,10 @@ fn main() {
             cheapest_distance = i;
             lowest_fuel_cost = fuel_cost;
         }
-    }*/
+    }
+
+    println!(
+        "Chepeast distance: {}, fuel: {}",
+        cheapest_distance, lowest_fuel_cost
+    );
 }
