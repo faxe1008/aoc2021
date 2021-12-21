cb008afc88cadf801bf66d19da1b60f40dfdfec5
blatzfab <fabian.blatz@hansgrohe.com>
2021-12-21 14:05:34 +0100

    Solution two for lantern fish, alias the SPEEEEEEEEEEEEEEEEEEEEEEEEEEEED

--- 6-lantern-fish/src/main.rs
+++ 6-lantern-fish/src/main.rs
@@ -1,42 +1,6 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
-
-#[derive(Clone)]
-struct LanternFish {
-    reproduction_timer: u32,
-}
-
-impl Default for LanternFish {
-    fn default() -> Self {
-        LanternFish {
-            reproduction_timer: 8,
-        }
-    }
-}
-
-impl std::fmt::Debug for LanternFish {
-    fn fmt(&self, _: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        print!("{}", self.reproduction_timer);
-        Ok(())
-    }
-}
-
-impl LanternFish {
-    fn new(timer: u32) -> Self {
-        LanternFish {
-            reproduction_timer: timer,
-        }
-    }
-    fn tick_day(&mut self) -> Option<LanternFish> {
-        if self.reproduction_timer == 0 {
-            self.reproduction_timer = 6;
-            Some(LanternFish::default())
-        } else {
-            self.reproduction_timer -= 1;
-            None
-        }
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -45,23 +9,33 @@ fn main() {
     }
 
     let text = fs::read_to_string(&args[1]).expect("Error reading file");
-    let mut fish_swarm: Vec<LanternFish> = text
+    let mut fish_swarm: Vec<u8> = text
         .trim()
         .split(',')
-        .map(|x| LanternFish::new(x.parse().expect("Error converting to int")))
+        .map(|x| x.parse().expect("Error converting to int"))
         .collect();
-    let generations: usize = 80;
 
-    for i in 1..=generations {
-        let mut next_state: Vec<LanternFish> = Vec::new();
-        for fish in fish_swarm.iter_mut() {
-            if let Some(new_fish) = fish.tick_day() {
-                next_state.push(new_fish);
-            }
-            next_state.push(fish.clone());
-        }
-        println!("Day {}: {:?}", i, next_state);
-        fish_swarm = next_state.clone();
+    // set up the table of how many fish have which timer
+    let mut fish_map: HashMap<u8, usize> = HashMap::new();
+    for i in 0..=8 {
+        fish_map.insert(i, fish_swarm.iter().filter(|&&y| y == i).count());
     }
-    println!("There are {} fish", fish_swarm.len());
+
+    let generations: usize = 256;
+    for i in 1..=generations {
+        let mut next_fish_map: HashMap<u8, usize> = HashMap::new();
+
+        *next_fish_map.entry(6).or_insert(0) += fish_map.get(&0).unwrap_or(&0); // reset
+        *next_fish_map.entry(8).or_insert(0) += fish_map.get(&0).unwrap_or(&0); // new borns
+
+        for x in 0..=8 {
+            *next_fish_map.entry(x).or_insert(0) += fish_map.get(&(x + 1)).unwrap_or(&0);
+            //Next gens
+        }
+
+        fish_map = next_fish_map.clone();
+        println!("Day {}: {:#?}", i, fish_map);
+    }
+    let fish_count: usize = fish_map.into_iter().map(|(_timer, occ)| occ).sum();
+    println!("There are {} fish", fish_count);
 }
