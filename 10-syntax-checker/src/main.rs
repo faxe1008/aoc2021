c74811e32478d51548fec7058947527f805216ae
blatzfab <fabian.blatz@hansgrohe.com>
2021-12-22 14:53:38 +0100

    Solution two for syntax checker

--- 10-syntax-checker/src/main.rs
+++ 10-syntax-checker/src/main.rs
@@ -21,6 +21,7 @@ impl SyntaxChecker {
     }
 
     fn check(&mut self, text: &str) -> Vec<char> {
+        self.symbol_stack.clear();
         let mut errors = Vec::new();
         let all_signs = text.trim().chars();
 
@@ -41,13 +42,21 @@ impl SyntaxChecker {
                 if stack_top.is_some() && stack_top.unwrap() != expected_stack_sym {
                     // we have an error
                     let expected_current_symbol = syntax_mapping.get(&stack_top.unwrap()).unwrap();
-                    println!("Expected {}, but found {}", expected_current_symbol, sign);
+                    //println!("Expected {}, but found {}", expected_current_symbol, sign);
                     errors.push(sign);
                     break;
                 }
             }
         }
         errors
+    }
+
+    fn has_errors(&mut self, text: &str) -> bool {
+        !self.check(text).is_empty()
+    }
+
+    fn has_completed(&self) -> bool {
+        self.symbol_stack.is_empty()
     }
 
     fn get_error_score(&self, errors: &Vec<char>) -> u32 {
@@ -62,6 +71,32 @@ impl SyntaxChecker {
             })
             .sum()
     }
+
+    fn autocomplete(&mut self) -> Vec<char> {
+        let mut missing_symbols = Vec::new();
+        let syntax_mapping: HashMap<char, char> =
+            HashMap::from([('(', ')'), ('{', '}'), ('<', '>'), ('[', ']')]);
+        println!("Attempting to complete: {:?}", self.symbol_stack);
+        while let Some(stack_top) = self.symbol_stack.pop() {
+            missing_symbols.push(syntax_mapping.get(&stack_top).unwrap().clone());
+        }
+        missing_symbols
+    }
+
+    fn get_autocomplete_score(&self, missing_symbols: &Vec<char>) -> u64 {
+        let mut score = 0;
+        for sym in missing_symbols {
+            score *= 5;
+            score += match sym {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => 0,
+            }
+        }
+        score
+    }
 }
 
 fn main() {
@@ -73,15 +108,25 @@ fn main() {
     let file = File::open(&args[1]).expect("Error opening file");
     let reader = BufReader::new(file);
 
-    let mut total_score = 0;
+    let mut all_scores: Vec<u64> = Vec::new();
     for line in reader.lines() {
         let line_text = line.unwrap();
         let mut syntax_checker = SyntaxChecker::default();
         let errors = syntax_checker.check(&line_text);
-        if errors.is_empty() {
+        if syntax_checker.has_errors(&line_text) || syntax_checker.has_completed() {
             continue;
         }
-        total_score += syntax_checker.get_error_score(&errors);
+        let missing_sybols = syntax_checker.autocomplete();
+        let score = syntax_checker.get_autocomplete_score(&missing_sybols);
+        println!(
+            "{:?} has the score {}",
+            missing_sybols.iter().cloned().collect::<String>(),
+            score
+        );
+        all_scores.push(score);
     }
-    println!("Total error score is: {}", total_score);
+
+    all_scores.sort();
+    let middle_score = all_scores.get(all_scores.len() / 2).unwrap();
+    println!("Middle score is: {}", middle_score);
 }
